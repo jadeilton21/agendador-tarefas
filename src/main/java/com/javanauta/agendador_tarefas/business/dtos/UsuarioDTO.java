8562d0a7a8b06a007c71f624da5c2ad73629d18a
jadeilton <jadeiltondm@gmail.com>
2025-09-04 18:42:01 -0300

    Adicionadas Configurações de Segurança

---
+++ src/main/java/com/javanauta/agendador_tarefas/business/dtos/UsuarioDTO.java
@@ -0,0 +1,22 @@
+package com.javanauta.agendador_tarefas.business.dtos;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+
+
+
+    private String email;
+    private String senha;
+
+
+}
